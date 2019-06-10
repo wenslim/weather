3e247249daaa576b938e68f84b36d2f6eefd43ca
Wenslim <396902854@qq.com>
2019-06-10 05:56:32 +0000

    Apply fixes from StyleCI

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
+/*
+ * This file is part of the wenslim/weather.
+ *
+ * (c) wenslim <396902854@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 echo 1234;
